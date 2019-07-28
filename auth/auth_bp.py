40a4eb3be85359b5fee1fe192f4d26209212535e
FarinHeiT <farinheit.lis@gmail.com>
2019-07-28 11:50:00 +0300

    Basic authentication, login template and redirection after auth

--- auth/auth_bp.py
+++ auth/auth_bp.py
@@ -1,10 +1,26 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request, flash, redirect, url_for
 from app import db
+from models import User
 
 
 auth = Blueprint('auth', __name__, template_folder='templates')
 
 
-@auth.route('/')
-def index():
-	return 'auth bp works!'
+@auth.route('/login', methods=('GET', 'POST'))
+def login():
+	
+	if request.method == 'POST':
+		username = request.form.get('username')
+		password = request.form.get('password')
+
+		# Retrieve  user obj from DB, if doesn't exist - it equals to None
+		user = User.query.filter(User.username==username).first()
+
+		# If wrong username or pwd -> flash error msg
+		if not user or user.password != password:
+			flash('Incorrect Username or Password. Please try again.')
+		else:
+			return redirect(url_for('index'))
+
+	# If GET request - just render template
+	return render_template('login.html')
