901fdef4cae1a63543eb6d33c3abb1ee3bced854
FarinHeiT <farinheit.lis@gmail.com>
2019-07-29 10:05:19 +0300

    Structure refactoring

--- app.py
+++ app.py
@@ -4,6 +4,9 @@ from config import Config
 from flask_migrate import Migrate
 from flask_login import LoginManager
 
+# The app can be launched through "flask run" or 
+# directrly through running "main.py"
+
 app = Flask(__name__)
 app.config.from_object(Config)
 
@@ -16,3 +19,8 @@ migrate = Migrate(app, db)
 # login object
 login_manager = LoginManager()
 login_manager.init_app(app)
+
+# We load views and blueprints here because of circular imports  
+from auth.auth_bp import auth
+import view
+app.register_blueprint(auth, url_prefix='/auth')
--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from app import app
 from auth.auth_bp import auth
 import view
 
+
 app.register_blueprint(auth, url_prefix='/auth')
 
 if __name__ == '__main__':
